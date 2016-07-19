48e29b4c984b09dc9a59fd699d006cef0b0b91d7
sebas <sebastiasebas@gmail.com>
2016-07-19 18:09:13 +0200

    v1.0.l llums random, start whatsapp

--- 1_sem.js
+++ 1_sem.js
@@ -1,7 +1,6 @@
 
 // Semafor manegat des un browser via node en raspberry.
-// Sebastia Altemir, Juny de 2016.
-// sebastiasebas@gmail.com
+//     Sebastia Altemir, Juny de 2016,  sebastiasebas@gmail.com
 
 // Versions ( displayed via 'myVersio')
 // 1.0.a - inici del codi amb express
@@ -14,31 +13,44 @@
 // 1.0.h - sequenciador : intermitent verd
 // 1.0.i - fer servir CONST per definir el pin de cada color.
 // 1.0.j - sequenciador : intermitent tres llums
+// 1.0.k - seq RAV
+// 1.0.l - ramdom color
+
 
 // Conexionat del GPIO :
-// el 'yellow' correspon al pin numero 15, cable gris  - k_Groc
-// el 'red'    correspon al pin numero 16, cable negre - k_Vermell
-// el 'green'  correspon al pin numero 18, cable blanc - k_Verd
+//     el 'yellow' correspon al pin numero 15, cable gris  - k_Groc
+//     el 'red'    correspon al pin numero 16, cable negre - k_Vermell
+//     el 'green'  correspon al pin numero 18, cable blanc - k_Verd
 
-// Engegar amb 'sudo node 1-sem.js'
-// Apuntar el browser a 192.168.1.123:1212
+// How to run the app :
+//     Server : engegar amb 'sudo node 1-sem.js'
+//     Client intern : apuntar el browser a 192.168.1.123:1212
+//     Client extern : http://88.18.117.86:9009/
 
 // Comandes reconegudes :
-// http://192.168.1.123:1212
-// http://192.168.1.123:1212/activar-sequencia-VAR
-// http://192.168.1.123:1212/parar-sequencia-VAR
-// http://192.168.1.123:1212/mostrar-foto
-// http://192.168.1.123:1212/hacer-foto
+//     http://192.168.1.123:1212
+//     http://192.168.1.123:1212/activar-sequencia-VAR
+//     http://192.168.1.123:1212/parar-sequencia-VAR
+//     http://192.168.1.123:1212/mostrar-foto
+//     http://192.168.1.123:1212/hacer-foto
 
-// Documentation :
+// Documentacio :
 //     filesystem          : https://nodejs.org/api/fs.html
 //     tancar ordenadament : http://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits/14032965#14032965
 
 // Temes pendents :
-//     obrir el router per accedir des Moscu - http://usuaris.tinet.cat/sag/rspi3.htm#rspi_obrir_ports
-//     obrir el router per actualitzar el software via SSH al port 22
+//     (*) abans de fer una foto, esborrar la anterior (encara en tenim el nom)
+//     (*) cron - tasca "netejar"
+//     (*) obrir el router per accedir el client des Moscu - http://usuaris.tinet.cat/sag/rspi3.htm#rspi_obrir_ports
+//     (*) obrir el router per actualitzar el software via SSH al port 22
+//     (*) provar des un mobil - la pantalla es molt petita !
+//     (*) tancar l'aplicacio des el client
+//     (*) engegar l'aplicacio en engegar el Raspi
+//     (*) enviat missatge whatsapp (amb una imatge) des el browser
+//     (*) identificar el usuari al client
+//     (*) random(3 bits) -> 8 valors
 
-// Proves de correcte funcionament : 5_llista_de_proves.txt
+// Pla de proves de correcte funcionament : 5_llista_de_proves.txt
 
 // =========================================================================
 
@@ -70,14 +82,14 @@ var PinEncendido    = k_Vermell ;       // to cycle thru color sequence.
 var myIntervalObject ;                  // used by clearInterval.
 var myIntervalValue = 1000 ;            // slow = 3000, normal = 1000, fast = 500.
 var szResultat      = '' ;              // console and client return string
-var myVersio        = 'v1.0.j' ;        // version identifier
+var myVersio        = 'v1.0.l' ;        // version identifier
 var png_File        = '/home/pi/semafor/public/images/webcam/webcam.png' ; // created by python
 
 
 // configuracio :
 // ==============
 
-     app.set( 'mPort', process.env.PORT || 1212 ) ;      // save port to use in APP var
+     app.set( 'mPort', process.env.PORT || 1212 ) ;      // save port to use in APP var ; shall use 1212 (see docu)
 //    app.disable( 'etag' ) ;                             // try to prevent 304
 
 // tell Express application to load and server static files (if there any) from public folder:
@@ -140,8 +152,8 @@ function apagarLuz( miPin ) { //
 
 function aturar_Llums_i_Tot() {
 
-     szResultat = '>>> Aturar totes les llums i els temporitzadors.' ;
-     console.log( szResultat ) ;
+//     szResultat = '>>> Aturar totes les llums i els temporitzadors.' ;
+//     console.log( szResultat ) ;
 
      clearInterval( myIntervalObject ) ;
      apagarLuz( k_Groc ) ;
@@ -167,13 +179,29 @@ function cambiarLuces() {
 } ; // cambiarLuces()
 
 
+function Gen_Random_0to2() {
+
+     var iFP = Math.random() ;                       // generate a (floating point) random number between [0 and 1)
+//     console.log( 'I = [' + iFP + '].' ) ;
+
+     var jINT = Math.floor( iFP * 3 ) ;              // integer value [0..2]
+//     console.log( 'J = [' + jINT + '].' ) ;
+
+     return jINT ;                                   // return a value 
+
+} ; //
+
+
 function Mi_Sequenciador() {
 
 // sequencies implementades :
-//      1 - 2 : llum verd on and off
-//      3 - 4 : llum groc on and off
-//      5 - 6 : llum vermell on and off
-//      7 - 8 : 3 llums on and off
+//      1 - 2       : llum verd on and off
+//      3 - 4       : llum groc on and off
+//      5 - 6       : llum vermell on and off
+//      7 - 8       : 3 llums on and off
+//      9 - 10 - 11 : verde - amarillo - rojo (VAR)
+//     12 - 13 - 14 : rojo - amarillo - verde (RAV)
+//     15           : random color
 
      console.log( '>>> Sequenciador, q (%s), T (%s).', Q_sequenciador, myIntervalValue ) ;
 
@@ -218,14 +246,56 @@ function Mi_Sequenciador() {
                encenderLuz( k_Groc ) ;
                encenderLuz( k_Vermell ) ;
                Q_sequenciador = 8 ;
-          break ; // case 5
+          break ; // case 7
 
           case 8:
                apagarLuz( k_Verd ) ;
                apagarLuz( k_Groc ) ;
                apagarLuz( k_Vermell ) ;
                Q_sequenciador = 7 ;
-          break ; // case 6
+          break ; // case 8
+
+          case 9:
+               encenderLuz( k_Verd ) ;
+               apagarLuz( k_Vermell ) ;
+               Q_sequenciador = 10 ;
+          break ; // case 9
+
+          case 10:
+               encenderLuz( k_Groc ) ;
+               apagarLuz( k_Verd ) ;
+               Q_sequenciador = 11 ;
+          break ; // case 10
+
+          case 11:
+               encenderLuz( k_Vermell ) ;
+               apagarLuz( k_Groc ) ;
+               Q_sequenciador = 9 ;
+          break ; // case 11
+
+          case 12:
+               encenderLuz( k_Vermell ) ;
+               apagarLuz( k_Verd ) ;
+               Q_sequenciador = 13 ;
+          break ; // case 12
+
+          case 13:
+               encenderLuz( k_Groc ) ;
+               apagarLuz( k_Vermell ) ;
+               Q_sequenciador = 14 ;
+          break ; // case 13
+
+          case 14:
+               encenderLuz( k_Verd ) ;
+               apagarLuz( k_Groc ) ;
+               Q_sequenciador = 12 ;
+          break ; // case 14
+
+          case 15:
+               var iNum = Gen_Random_0to2() ;
+               console.log( '>>> random color (%s).', iNum ) ;
+          break ; // case 15
+
 
           default:
                Q_sequenciador = 0 ;
@@ -293,7 +363,11 @@ app.post( '/menu_cerrar_aplicacion', function ( req, res ) {
 
 app.get( '/identificar', function (req, res) {
 
-     szResultat = '+++ app SEM JA. Versio ['+myVersio+'], HN ['+app.get('appHostname')+'], TimeStamp ['+(new Date).hhmmss()+'].' ;
+     szResultat  = '+++ app SEM JA. ' ;
+     szResultat += 'Versio [' + myVersio + ']. ' ;
+     szResultat += 'HN [' + app.get( 'appHostname' )+ ']. ' ;
+     szResultat += 'GYR [' + k_Verd + '/' + k_Groc + '/' + k_Vermell + ']. ' ;
+     szResultat += 'TimeStamp [' + (new Date).hhmmss() + '].' ;
      console.log( szResultat ) ;
      res.status( 200 ).send( szResultat ) ; 
 
@@ -303,24 +377,23 @@ app.get( '/identificar', function (req, res) {
 app.post( '/menu_apagar_llum/Color=:res_color_llum', function ( req, res ) {
 
      var Apagar_Llum_Color = req.params.res_color_llum ;
-     console.log( '>>> Menu apagar llum (%s).', Apagar_Llum_Color ) ;
+     szResultat = '>>> Menu apagar llum (' + Apagar_Llum_Color + '). ' ;
 
-     if ( Apagar_Llum_Color == 'verd' ) {
+//     if ( Apagar_Llum_Color == 'verd' ) {
 //          apagarLuz( k_Verd ) ;
-          aturar_Llums_i_Tot() ;
-          szResultat = '+++ llum verda apagada.' ;
-     } else if ( Apagar_Llum_Color == 'groc' ) {
+//          szResultat = '+++ llum verda apagada.' ;
+//     } else if ( Apagar_Llum_Color == 'groc' ) {
 //          apagarLuz( k_Groc ) ;
-          aturar_Llums_i_Tot() ;
-          szResultat = '+++ llum groc apagat.' ;
-     } else if ( Apagar_Llum_Color == 'vermell' ) {
+//          szResultat = '+++ llum groc apagat.' ;
+//     } else if ( Apagar_Llum_Color == 'vermell' ) {
 //          apagarLuz( k_Vermell ) ;
-          aturar_Llums_i_Tot() ;
-          szResultat = '+++ llum vermell apagat.' ;
-     } else {
-          szResultat = '--- color (' + Apagar_Llum_Color + ') no reconegut.' ;
-     } ;
+//          szResultat = '+++ llum vermell apagat.' ;
+//     } else {
+//          szResultat = '--- color (' + Apagar_Llum_Color + ') no reconegut.' ;
+//     } ;
 
+     aturar_Llums_i_Tot() ;
+     szResultat += '+++ tots els llums apagats.' ;
      console.log( szResultat ) ;
      res.status( 200 ).send( szResultat ) ; 
 
@@ -356,7 +429,35 @@ app.post( '/menu_encendre_llum/Color=:res_color_llum', function ( req, res ) {
 } ) ; // menu encendre llum
 
 
-app.get( '/foto_esborrar', function (req, res) {
+app.post( '/enviar_msg_whatsapp/ParamTfNum=:req_tf_num', function (req, res) {
+
+// agafar parametres : num tf i texte
+
+     var WhatsApp_Tf_Number = req.params.req_tf_num ;
+     console.log( '>>> Menu enviar msg WhatsApp via python. Tf param is (' + WhatsApp_Tf_Number + '). ' ) ;
+
+var python_options = {
+  mode: 'text',
+  pythonPath: '/usr/bin/python',
+  pythonOptions: ['-u'],
+  scriptPath: '/usr/local/bin',
+  args: ['demos  -c /usr/local/bin/mydetails   -s 34638015371  SEMnodeMsg']
+} ;
+
+//     PythonShell.run( 'yowsup-cli', function( err, results ) {
+     PythonShell.run( '/usr/local/bin/yowsup-cli', function( err, results ) {
+          if ( err ) throw err ;
+          console.log( '(+) Snd WhatsApp Python results are (%j).', results ) ; // results is an array consisting of messages collected during execution
+          var sndRC = String( results ) ;                           // convert to string
+
+          szResultat = '+++ whatsapp msg sent. RC ('+ sndRC + ').' ;
+          res.status( 200 ).send( szResultat ) ; 
+     } ) ; // run
+
+} ) ; // enviar mensage whatsapp
+
+
+app.get( '/foto_esborrar_fitxer', function (req, res) {
 
      borrar_Fichero( png_File ) ;
      szResultat = '+++ file (' + png_File + ') deleted successfully' ;
@@ -378,10 +479,10 @@ var python_options = {
 
      console.log( '>>> Menu hacer foto via Python.' ) ;
 
-// esborrem primer la foto anterior que hem guardat a png_File ?
+// esborrem primer la foto anterior - tenim el seu nom a png_File ?
 
      PythonShell.run( '2_foto.py', function( err, results ) {
-          if ( err ) throw err;
+          if ( err ) throw err ;
           console.log( '(+) Python results are (%j).', results ) ; // results is an array consisting of messages collected during execution
           png_File = String( results ) ;                           // convert to string
 
@@ -399,7 +500,7 @@ app.get( '/mostrar_foto', function (req, res) {
      var imatge = fs.readFileSync( png_File ) ;
 
      res.writeHead( 200, { 'Content-Type': 'text/html' } ) ;
-     res.write( '<img src="data:image/png;base64,' ) ;
+     res.write( '<img id="imatge_webcam" src="data:image/png;base64,' ) ;
      res.write( new Buffer(imatge).toString( 'base64' ) ) ;
      res.end( '"/>' ) ;
 
@@ -430,60 +531,60 @@ app.post( '/modificar_interval/Periode=:res_nou_periode', function (req, res) {
 } ) ; // modificar interval intermitencies
 
 
-app.get( '/activar-sequencia-VAR', function (req, res) { 
-
-     szResultat = '+++ Activar sequencia VAR, periode (' + myIntervalValue + ').' ;
-
-//     Q_sequenciador = 100 ;
-     myIntervalObject = setInterval( cambiarLuces, myIntervalValue ) ; // schedules repeated execution of 'callback' every 'delay' milliseconds.
-
-     console.log( szResultat ) ;
-     res.status( 200 ).send( szResultat ) ; 
-
-} ) ; // activar VAR
-
-
-app.get( '/parar-sequencia-VAR', function (req, res) { 
-
-     aturar_Llums_i_Tot() ;
-
-     szResultat = '(---) Parar sequencia VAR. Clear Interval.' ;
-     console.log( szResultat ) ;
-     res.status( 200 ).send( szResultat ) ; 
-
-} ) ; // parar VAR
-
+// Parametres d'entrada al SEQUENCIADOR, determinats pel web client
 
 app.post( '/menu_engegar_sequencia/Tipus=:res_tipus_sequencia', function (req, res) { 
 
      var Nou_Tipus_Sequencia = req.params.res_tipus_sequencia ;
      szResultat = '>>> Menu engegar sequencia (' + Nou_Tipus_Sequencia + '). ' ;
 
-     if ( Nou_Tipus_Sequencia == 'intermitent_verd' ) {
-          szResultat += '+++ color verd. T (' + myIntervalValue + ').' ;
-          Q_sequenciador = 1 ;
-          myIntervalObject = setInterval( Mi_Sequenciador, myIntervalValue ) ; // schedules repeated execution of 'callback' every 'delay' milliseconds.
-     } else {
-          if ( Nou_Tipus_Sequencia == 'intermitent_groc' ) {
+     switch ( Nou_Tipus_Sequencia ) {
+
+          case 'intermitent_verd':
+               szResultat += '+++ color verd. T (' + myIntervalValue + ').' ;
+               Q_sequenciador = 1 ;
+          break ;
+
+          case 'intermitent_groc':
                szResultat += '+++ color groc. T (' + myIntervalValue + ').' ;
                Q_sequenciador = 3 ;
-               myIntervalObject = setInterval( Mi_Sequenciador, myIntervalValue ) ; // idem
-          } else {
-               if ( Nou_Tipus_Sequencia == 'intermitent_vermell' ) {
-                    szResultat += '+++ color vermell. T (' + myIntervalValue + ').' ;
-                    Q_sequenciador = 5 ;
-                    myIntervalObject = setInterval( Mi_Sequenciador, myIntervalValue ) ; // idem
-               } else {
-                    if ( Nou_Tipus_Sequencia == 'intermitent_tres_llums' ) {
-                         szResultat += '+++ 3 colors. T (' + myIntervalValue + ').' ;
-                         Q_sequenciador = 7 ;
-                         myIntervalObject = setInterval( Mi_Sequenciador, myIntervalValue ) ; // idem
-                    } else {
-                         szResultat += '--- valor (' + Nou_Tipus_Sequencia + ') no reconegut.' ;
-                    } ; // not 3 llums
-               } ; // not RED
-          } ; // not YELLOW
-     } ; // not VERD
+          break ;
+
+          case 'intermitent_vermell':
+               szResultat += '+++ color vermell. T (' + myIntervalValue + ').' ;
+               Q_sequenciador = 5 ;
+          break ;
+
+          case 'intermitent_tres_llums':
+               szResultat += '+++ 3 colors. T (' + myIntervalValue + ').' ;
+               Q_sequenciador = 7 ;
+          break ;
+
+          case 'sequencia_VAR':
+               szResultat += '+++ sequencia VAR. T (' + myIntervalValue + ').' ;
+               Q_sequenciador = 9 ;
+          break ;
+
+          case 'sequencia_RAV':
+               szResultat += '+++ sequencia RAV. T (' + myIntervalValue + ').' ;
+               Q_sequenciador = 12 ;
+          break ;
+
+          case 'sequencia_random':
+               szResultat += '+++ sequencia Random. T (' + myIntervalValue + ').' ;
+               Q_sequenciador = 15 ;
+          break ;
+
+          default:
+               szResultat += '--- valor (' + Nou_Tipus_Sequencia + ') no reconegut.' ;
+               Q_sequenciador = 0 ;
+
+     } ; // switch Nou_Tipus_Sequencia
+
+
+     if ( Q_sequenciador != 0 ) {
+          myIntervalObject = setInterval( Mi_Sequenciador, myIntervalValue ) ; // schedules repeated execution of 'callback' every 'delay' mSg.
+     } ;
 
      console.log( szResultat ) ;
      res.status( 200 ).send( szResultat ) ; 
