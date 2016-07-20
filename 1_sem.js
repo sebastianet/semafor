59c0612781f02177c8dc28986571ed71e852946e
sebas <sebastiasebas@gmail.com>
2016-07-21 00:19:39 +0200

    v1.0.n enviem msg whatsapp des el browser client

--- 1_sem.js
+++ 1_sem.js
@@ -16,6 +16,7 @@
 // 1.0.k - seq RAV
 // 1.0.l - ramdom color
 // 1.0.m - enviem missatges whatsapp des python
+// 1.0.n - enviem missatges whatsapp des el browser client
 
 
 // Conexionat del GPIO :
@@ -25,8 +26,8 @@
 
 // How to run the app :
 //     Server : engegar amb 'sudo node 1-sem.js'
-//     Client intern : apuntar el browser a 192.168.1.123:1212
-//     Client extern : http://88.18.117.86:9009/
+//     Client intern : apuntar el browser a http://192.168.1.123:1212
+//     Client extern :                      http://88.18.117.86:9009
 
 // Comandes reconegudes :
 //     http://192.168.1.123:1212
@@ -36,7 +37,11 @@
 //     http://192.168.1.123:1212/hacer-foto
 
 // Documentacio :
+//     projecte "base"        : https://github.com/sebastianet/wCDT
+//     projecte actual        : https://github.com/sebastianet/semafor
+//     Valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> | Valid <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
 //     filesystem             : https://nodejs.org/api/fs.html
+//     logger                 : https://github.com/expressjs/morgan 
 //     tancar ordenadament    : http://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits/14032965#14032965
 //     call python from node  : https://github.com/extrabacon/python-shell
 //         rc = 1             : https://github.com/extrabacon/python-shell/issues/46
@@ -51,7 +56,7 @@
 //     (*) provar des un mobil - la pantalla es molt petita !
 //     (*) tancar l'aplicacio des el client
 //     (*) engegar l'aplicacio en engegar el Raspi
-//     (*) enviat missatge whatsapp (amb una imatge) des el browser
+//     (*) enviat missatge whatsapp amb una imatge des el browser
 //     (*) identificar el usuari al client
 //     (*) random(3 bits) -> 8 valors
 //     (*) fer LOGON() obligatori i fer una trassa de IPs que entren
@@ -71,7 +76,9 @@ var logger       = require( 'morgan' ) ;             // logging middleware
 var fs           = require( 'fs' ) ;                 // get JPG or PNG
 
 var gpio         = require( 'rpi-gpio' ) ;           // GPIO pin access
-var PythonShell  = require( 'python-shell' ) ;       // to send commands to python
+var PythonShell  = require( 'python-shell' ) ;       // send commands to python
+
+var bodyParser   = require( "body-parser" ) ;
 
 
 // les meves constants :
@@ -84,11 +91,10 @@ const k_Vermell  = 16 ;
 // =====================
 
 var Q_sequenciador  = 0 ;               // estat del sequenciador := aturat ;
-var PinEncendido    = k_Vermell ;       // to cycle thru color sequence.
 var myIntervalObject ;                  // used by clearInterval.
 var myIntervalValue = 1000 ;            // slow = 3000, normal = 1000, fast = 500.
 var szResultat      = '' ;              // console and client return string
-var myVersio        = 'v1.0.m' ;        // version identifier
+var myVersio        = 'v1.0.n' ;        // version identifier
 var png_File        = '/home/pi/semafor/public/images/webcam/webcam.png' ; // created by python
 
 
@@ -103,8 +109,10 @@ var png_File        = '/home/pi/semafor/public/images/webcam/webcam.png' ; // cr
      app.use( express.static( path.join(__dirname, 'public' ) ) );  // objects from the ./public/ uri will be served from .\public\ dir
      app.use( '/', express.static( __dirname + '/public' ) ) ;      // serve whatever is in the 'public' folder at the URL /public/:filename
 
+     app.use( bodyParser.urlencoded( { extended:true } ) ) ;
+
 // eina per depurar el programa
-     app.use( logger( 'dev' ) ) ;                     // https://github.com/expressjs/morgan - tiny (minimal), dev (developer), common (apache)
+     app.use( logger( 'dev' ) ) ;                         // tiny (minimal), dev (developer), common (apache)
 
 
 // definim algunes funcions :
@@ -171,26 +179,12 @@ function aturar_Llums_i_Tot() {
 } ; // aturar llums i temporitzador
 
 
-function cambiarLuces() {
-
-     console.log( '>>> Cambiar luces (%s).', PinEncendido ) ;
-     apagarLuz( PinEncendido ) ;
-     if ( PinEncendido == k_Groc ) { PinEncendido = k_Vermell } else {
-          if ( PinEncendido == k_Vermell ) { PinEncendido = k_Verd } else {
-               if ( PinEncendido == k_Verd ) { PinEncendido = k_Groc } ;
-          } ;
-     } ;
-     encenderLuz( PinEncendido ) ;
-
-} ; // cambiarLuces()
-
-
-function Gen_Random_0to2() {
+function Gen_Random_0toMax( limitMax ) {
 
      var iFP = Math.random() ;                       // generate a (floating point) random number between [0 and 1)
 //     console.log( 'I = [' + iFP + '].' ) ;
 
-     var jINT = Math.floor( iFP * 3 ) ;              // integer value [0..2]
+     var jINT = Math.floor( iFP * ( limitMax + 1 ) ) ;              // integer value [0..limitMax]
 //     console.log( 'J = [' + jINT + '].' ) ;
 
      return jINT ;                                   // return a value 
@@ -207,7 +201,10 @@ function Mi_Sequenciador() {
 //      7 - 8       : 3 llums on and off
 //      9 - 10 - 11 : verde - amarillo - rojo (VAR)
 //     12 - 13 - 14 : rojo - amarillo - verde (RAV)
-//     15           : random color
+//     15           : random color [0..2]
+//     16           : random color [0..7]
+
+var iNum = 0 ;
 
      console.log( '>>> Sequenciador, q (%s), T (%s).', Q_sequenciador, myIntervalValue ) ;
 
@@ -298,10 +295,14 @@ function Mi_Sequenciador() {
           break ; // case 14
 
           case 15:
-               var iNum = Gen_Random_0to2() ;
-               console.log( '>>> random color (%s).', iNum ) ;
+               iNum = Gen_Random_0toMax( 2 ) ;                       // get a velue in [0..2]
+               console.log( '>>> random color (%s) in [0..2].', iNum ) ;
           break ; // case 15
 
+          case 16:
+               iNum = Gen_Random_0toMax( 7 ) ;                       // get a velue in [0..7]
+               console.log( '>>> random color (%s) in [0..7].', iNum ) ;
+          break ; // case 16
 
           default:
                Q_sequenciador = 0 ;
@@ -435,12 +436,8 @@ app.post( '/menu_encendre_llum/Color=:res_color_llum', function ( req, res ) {
 } ) ; // menu encendre llum
 
 
-app.post( '/enviar_msg_whatsapp/ParamTfNum=:req_tf_num', function (req, res) {
-
-// agafar parametres : num tf i texte
-
-     var WhatsApp_Tf_Number = req.params.req_tf_num ;
-     console.log( '>>> Menu enviar msg WhatsApp via python. Tf param is (' + WhatsApp_Tf_Number + '). ' ) ;
+// app.post( '/enviar_msg_whatsapp/ParamTfNum=:req_tf_num', function ( req, res ) {
+app.post( '/enviar_msg_whatsapp', function ( req, res ) {
 
 var python_options = {
   mode: 'text',
@@ -449,15 +446,36 @@ var python_options = {
   scriptPath: '/usr/local/bin',
 //  args: [ 'demos',  '-c', '/usr/local/bin/mydetails',  '-s', '34638015371',  'Sebas : ho envio des el NODEJS' ]
 //  args: [ 'demos',  '-c', '/usr/local/bin/mydetails',  '-s', '34633611757',  'Albert : ho envio des el NODEJS' ]
-  args: [ 'demos',  '-c', '/usr/local/bin/mydetails',  '-s', '34616115628',  'Luis : enviado desde NODEJS' ]
+//  args: [ 'demos',  '-c', '/usr/local/bin/mydetails',  '-s', '34670559696',  'JALL : enviat des NODEJS al Raspi' ]
+//  args: [ 'demos',  '-c', '/usr/local/bin/mydetails',  '-s', '34616115628',  'Luis : enviado desde NODEJS' ]
+
+  args: [ 'demos',  '-c', '/usr/local/bin/mydetails',  '-s', '34666777888',  'Sebas : ho envio des el NODEJS' ]
+
 } ;
 
-     PythonShell.run( 'yowsup-cli', python_options, function( err, results ) {
-          if ( err ) throw err ;
-          console.log( '(+) Snd WhatsApp Python results are (%j).', results ) ; // results is an array consisting of messages collected during execution
-          var sndRC = String( results ) ;                           // convert to string
+     console.log( 'posted ' + JSON.stringify( req.body ) ) ;
 
-          szResultat = '+++ whatsapp msg sent. RC ('+ sndRC + ').' ;
+// agafar num tf i texte del BODY
+
+     var WhatsApp_Tf_Number = req.body.wdtel ;
+     var WhatsApp_Msg_Text  = req.body.wdtxt ;
+
+     console.log( '>>> Menu enviar msg WhatsApp via python. Tf REQ param is (' + WhatsApp_Tf_Number + '). ' ) ;
+
+     console.log( '>>> Menu enviar msg WhatsApp via python. +++ ARGS is (' + python_options.args + '). ' ) ;
+     python_options.args[4] = WhatsApp_Tf_Number ;
+     python_options.args[5] = WhatsApp_Msg_Text ;
+     console.log( '>>> Menu enviar msg WhatsApp via python. --- ARGS is (' + python_options.args + '). ' ) ;
+
+     PythonShell.run( 'yowsup-cli', python_options, function( err, results ) {
+
+//          console.log( '(+) Snd WhatsApp Python ERR are (%j).', err ) ;
+//          console.log( JSON.stringify( err ) ) ;
+//          if ( err ) throw err ;
+          var miRC = err.exitCode ;
+          szResultat = '(+) Snd WhatsApp Python RC (' + miRC + ').' ;
+
+          console.log( szResultat ) ;
           res.status( 200 ).send( szResultat ) ; 
      } ) ; // run
 
@@ -507,7 +525,7 @@ app.get( '/mostrar_foto', function (req, res) {
      var imatge = fs.readFileSync( png_File ) ;
 
      res.writeHead( 200, { 'Content-Type': 'text/html' } ) ;
-     res.write( '<img id="imatge_webcam" src="data:image/png;base64,' ) ;
+     res.write( '<img id="imatge_webcam" width="320" height="240" src="data:image/png;base64,' ) ;
      res.write( new Buffer(imatge).toString( 'base64' ) ) ;
      res.end( '"/>' ) ;
 
