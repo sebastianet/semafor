14955447b2c253b9187ed45764a86f3c3c34b123
sebas <sebastiasebas@gmail.com>
2016-07-24 23:16:04 +0200

    v1.0.q.2 separar client.js

---
+++ 7_random.js
@@ -0,0 +1,6 @@
+
+     i = Math.random() ;   // generate a (floating point) random number between [0 and 1)
+     console.log( 'I = [' + i + '].' ) ;
+
+     j = Math.floor( i * 10 ) ; // integer value
+     console.log( 'J = [' + j + '].' ) ;
