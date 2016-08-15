5b101b5cee654fc1774647154059f9a90440db49
sebas <sebastiasebas@gmail.com>
2016-08-15 19:23:56 +0200

    v1.1.j - fer foto OK en rafaga

--- 2_foto.py
+++ 2_foto.py
@@ -4,13 +4,16 @@ import numpy
 import cv2
 # print "### CV2 version I have is " + cv2.__version__
 
-# to debug this code, use
-#      python  -m pdb  3-v-en-color.py
-
 # Fer una foto amb la webcam i posar-la en un fitxer.
 # Es crida des "1_sem.js"
 # Imprimim el nom del fitxer que sera el parametre de tornada
 # No imprimim res mes
+
+# Documentacio
+#     # http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
+
+# to debug this code, use
+#      python  -m pdb  3-v-en-color.py
 
 import time
 timestr = time.strftime("%Y%m%d_%H%M%S")
@@ -29,44 +32,31 @@ if not cap:
     sys.exit(1)
 
 if not cap.isOpened() :
-#     print '>>> do VideoCapture::open()'             # http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
+#     print '>>> do VideoCapture::open()'
     bRC = cap.open( 0 )
 #     print '+++ open() bRC is (' + str(bRC) + ').'
 
 # try using own values
 # print '>>> set W 320, H 240.'
-# cap.set( 3, 640 )     # 1280 1024 800 640 320
-# cap.set( 4, 480 )     # 1024  800 600 480 240
+cap.set( cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320 )     # 1280 1024 800 640 320
+cap.set( cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240 )    # 1024  800 600 480 240
 # cv2.waitKey( 3000 )
 
 if ( cap.isOpened() ):
-    
-#     print '>>> Capture a frame using cap.read()'
-    ret, frame = cap.read()  # VideoCapture::read returns a Boolean and a Numpy
+ 
+    for count in range(0,5):
 
-    if ret:     # proceed only if we have a frame
+        ret, frame = cap.read()  # VideoCapture::read returns a Boolean and a Numpy
 
-        # first, display some frame values
+        if ret:     # proceed only if we have a frame
+
+            if ( count == 4 ):
+                cv2.imwrite ( szFN, frame )
+                print szFN                   # set return parameter for NODE
         
-        frC = 0
-        frW = int ( cap.get(3) ) # width, float to integer  - 640 fixe
-        frH = int ( cap.get(4) ) # height, float to integer - 480 fixe
-#        frC = int ( cap.get(7) ) # CV_CAP_PROP_FRAME_COUNT "7" - see http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
-#         print '+++ got frame num (' + str(frC) + '), width (' + str(frW) + '), height (' + str(frH) + ').'
-        
-#         print 'Size {' + str(frame.size) + '}, type {' + str(frame.dtype) + '}, shape {' + str(frame.shape) + '}.'
-
-        cv2.waitKey(500)                # wait 500 ms
-
-#         print '>>> Write frame into file (' + szFN + ') using imWrite()'
-        print szFN
-        cv2.imwrite ( szFN, frame )
-        
-    else:
-        print '--- VideoCapture::read no ha anat be'
-
-# print '>>> exit.'
+        else:
+            print '--- VideoCapture::read no ha anat be'
 
 # When everything done, release the capture
 cap.release()                                # close the already opened camera
-
+cap = None
--- 4_foto_verbose.py
+++ 4_foto_verbose.py
@@ -1,19 +1,25 @@
+#! /usr/bin/env python
+
 import numpy
 print "### NUMPY version I have is " + numpy.__version__
 
 import cv2
-print "### CV2 version I have is " + cv2.__version__
-
-# to debug this code, use
-#      python  -m pdb  3-v-en-color.py
+print "### OPENCV version I have is " + cv2.__version__
 
 # Fer una foto amb la webcam i posar-la en un fitxer.
 # Canviem els parametres i mostrem els resultants
+
+# Documentacio
+#     http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
+
+# to debug this code, use
+#      python  -m pdb  3-v-en-color.py
 
 import time
 timestr = time.strftime("%Y%m%d_%H%M%S")
 print timestr
 
+szShortFN = 'webcam_' + timestr + '.jpeg' 
 szShortFN = 'webcam_' + timestr + '.png' 
 print 'Filename (' + szShortFN + ').'
 
@@ -27,44 +33,55 @@ if not cap:
     sys.exit(1)
 
 if not cap.isOpened() :
-    print '>>> do VideoCapture::open()'             # http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
+    sys.stderr.write('could not connect to camera! \n')
+    sys.exit(1)
+
+    print '>>> do VideoCapture::open()'  
     bRC = cap.open( 0 )
     print '+++ open() bRC is (' + str(bRC) + ').'
 
 # try using own values
-iW = 800
-iH = 600
-print '>>> set W ('+ str(iW) +'), H ('+ str(iH) +').'
-cap.set( 3, iW )     # 1280 1024 800 640 320
-cap.set( 4, iH )     # 1024  800 600 480 240
+iW = 320
+iH = 240
+
+print '>>> set Width ('+ str(iW) +'), H ('+ str(iH) +').'
+cap.set( cv2.cv.CV_CAP_PROP_FRAME_WIDTH, iW )     # 1280 1024 800 640 320
+cap.set( cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, iH )    # 1024  800 600 480 240
 cv2.waitKey( 3000 )
 
+# 1_sem.js uses 320 x 240 ;
 # Logitech + 800 x 600 -> width (640), height (480)
 
 if ( cap.isOpened() ):
     
-    print '>>> Capture a frame using cap.read()'
-    ret, frame = cap.read()  # VideoCapture::read returns a Boolean and a Numpy
+    for count in range(0,5):
 
-    if ret:     # proceed only if we have a frame
+        print '>>> ('+ str(count) +') Capture a frame using cap.read()'
+        ret, frame = cap.read()  # VideoCapture::read returns a Boolean and a Numpy
 
-        # first, display some frame values
+        if ret:     # proceed only if we have a frame
+
+            # first, display some frame values
         
-        frC = 0
-        frW = int ( cap.get(3) ) # width, float to integer  - 640 fixe
-        frH = int ( cap.get(4) ) # height, float to integer - 480 fixe
-        frC = int ( cap.get(7) ) # CV_CAP_PROP_FRAME_COUNT "7" - see http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html
-        print '+++ got frame num (' + str(frC) + '), width (' + str(frW) + '), height (' + str(frH) + ').'
-        
-        print 'Size {' + str(frame.size) + '}, type {' + str(frame.dtype) + '}, shape {' + str(frame.shape) + '}.'
+            frC = 0
+            frW = int ( cap.get(3) ) # width, float to integer  
+            frH = int ( cap.get(4) ) # height, float to integer
+            # frC = int ( cap.get(7) ) # CV_CAP_PROP_FRAME_COUNT "7"
+            print '+++ got frame num (' + str(frC) + '), width (' + str(frW) + '), height (' + str(frH) + ').'
+            
+            print 'Size {' + str(frame.size) + '}, type {' + str(frame.dtype) + '}, shape {' + str(frame.shape) + '}.'
 
-        cv2.waitKey(500)                # wait 500 ms
+            cv2.waitKey(200)                # wait N mseg
 
-        print '>>> Write frame into file (' + szFN + ') using imWrite()'
-        cv2.imwrite ( szFN, frame )
-        
-    else:
-        print '--- VideoCapture::read no ha anat be'
+            if ( count == 4 ):
+                print '>>> Write frame into file (' + szFN + ') using imWrite()'
+                cv2.imwrite ( szFN, frame )
+
+                # cv2.imwrite ( szFN, frame, [cv2.cv.CV_IMWRITE_PNG_COMPRESSION, 0] )
+                # cv2.imwrite ( szFN, frame, [cv2.cv.CV_IMWRITE_JPEG_QUALITY, 50] )
+       
+        else:
+            print '--- VideoCapture::read no ha anat be'
 
 print '>>> exit.'
 
